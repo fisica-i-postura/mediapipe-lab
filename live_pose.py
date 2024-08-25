2d862f7f306ad2b5403a43fd99709cc1aa8df9b8
Ignacio Dotta <ij.dotta@gmail.com>
2024-08-25 19:32:50 -0300

    impl live pose recognition

---
+++ live_pose.py
@@ -0,0 +1,27 @@
+import cv2
+import mediapipe as mp
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = pose.process(img_rgb)
+
+    if result.pose_landmarks:
+        mp_draw.draw_landmarks(img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        for id, lm in enumerate(result.pose_landmarks.landmark):
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
